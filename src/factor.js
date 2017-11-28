0d1700dc99fda0192be4094591e2fd4e29226944
Kate Haskell <fuzzcat@bactroid.net>
2017-11-28 09:34:20 -0500

    Refactored getFactors to use filter

--- src/factor.js
+++ src/factor.js
@@ -2,16 +2,8 @@ const {range} = require('lodash')
 
 const isFactor = (num, factor) => num % factor === 0
 
-const getFactors = num => {
-  let factors = []
-  range(1, num + 1)
-    .forEach(x => {
-      if (isFactor(num, x)) {
-        factors.push(x)
-      }
-    })
-  return factors
-}
+const getFactors = num =>
+  range(1, num + 1).filter(x => isFactor(num, x))
 
 module.exports = {
   getFactors
