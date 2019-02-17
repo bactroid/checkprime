741e99e71226bf68d96a93b1d4ca16f74d10ce23
Kate Haskell <fuzzcat@bactroid.net>
2019-02-17 08:41:21 -0500

    Replace nested ternary conditional with if/else if/else

--- src/prime.js
+++ src/prime.js
@@ -1,12 +1,16 @@
 const range = require('@ramda/range')
 const isFactor = num => (factor, index, array) => num % factor === 0
 const isInvalid = num => num < 2
-const isPrime = num =>
-  isInvalid(num)
-    ? null
-    : num === 2
-      ? true
-      : !range(2, Math.sqrt(num) + 1).some(isFactor(num))
+
+const isPrime = num => {
+  if (isInvalid(num)) {
+    return null
+  } else if (num === 2) {
+    return true
+  } else {
+    return !range(2, Math.sqrt(num) + 1).some(isFactor(num))
+  }
+}
 
 module.exports = {
   isPrime
